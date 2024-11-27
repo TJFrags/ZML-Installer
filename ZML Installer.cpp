b1fd76eaeef0f8ddae69066d72cab5063004a4d4
TJFrags <t.jenya11@gmail.com>
2024-11-27 23:57:52 +0200

    Add project files.

---
+++ ZML Installer.cpp
@@ -0,0 +1,12 @@
+﻿// ZML Installer.cpp : Defines the entry point for the application.
+//
+
+#include "ZML Installer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
